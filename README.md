2adaf285d8cd7b4350d5d09c84136757c70c0f72
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2020-09-23 14:36:34 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Campo minado
+
+Projeto de uma campo minado utilizando Flutter
+
+## Sobre
+
+Projeto feito para criar um campo minado utilizando o framework do Flutter.
+A camada de controle foi implementada na camada de modelo.
+
+## Screenshots
+
+<p float="left">
+  <img src="screenshots/screenshot1.png" width="160" />
+  <img src="screenshots/screenshot2.png" width="160" /> 
+  <img src="screenshots/screenshot3.png" width="160" />
+</p>
---
+++ lib/models/campo.dart
@@ -0,0 +1,91 @@
+import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:flutter/foundation.dart';
+
+class Campo {
+  final int linha;
+  final int coluna;
+  final List<Campo> vizinhos = [];
+
+  bool _aberto = false;
+  bool _marcado = false;
+  bool _minado = false;
+  bool _explodido = false;
+
+  Campo({
+    @required this.linha,
+    @required this.coluna,
+  });
+
+  void adicionarVizinho(Campo vizinho) {
+    final deltaLinha = (linha - vizinho.linha).abs();
+    final deltaColuna = (coluna - vizinho.coluna).abs();
+
+    if (deltaLinha == 0 && deltaColuna == 0) {
+      return;
+    }
+
+    if (deltaLinha <= 1 && deltaColuna <= 1) {
+      vizinhos.add(vizinho);
+    }
+  }
+
+  void abrir() {
+    if (_aberto) {
+      return;
+    }
+
+    _aberto = true;
+
+    if (_minado) {
+      _explodido = true;
+      throw ExplosaoException();
+    }
+
+    if (vizinhacaSegura) {
+      vizinhos.forEach((v) => v.abrir());
+    }
+  }
+
+  void revelarBomba() {
+    if (_minado) {
+      _aberto = true;
+    }
+  }
+
+  void minar() {
+    _minado = true;
+  }
+
+  void alternarMarcacao() {
+    _marcado = !_marcado;
+  }
+
+  void reiniciar() {
+    _aberto = false;
+    _marcado = false;
+    _minado = false;
+    _explodido = false;
+  }
+
+  bool get minado => _minado;
+
+  bool get explodido => _explodido;
+
+  bool get aberto => _aberto;
+
+  bool get marcado => _marcado;
+
+  bool get resolvido {
+    bool minadoEMarcado = minado && marcado;
+    bool seguroEMarcado = !minado && aberto;
+    return minadoEMarcado || seguroEMarcado;
+  }
+
+  bool get vizinhacaSegura {
+    return vizinhos.every((v) => !v._minado);
+  }
+
+  int get qtdeMinasNaVizinhanca {
+    return vizinhos.where((v) => v.minado).length;
+  }
+}
